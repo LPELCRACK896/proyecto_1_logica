54423205e8df990b2b0a5b1a22f9544ef410308a
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-08-03 13:38:17 -0600

    evaluador de clausulas

--- inciso_1/inciso1.py
+++ inciso_1/inciso1.py
@@ -2,6 +2,9 @@
 
 """ Recibe [p, q, r] ,'pqr'"""
 from collections import namedtuple
+from logging.config import valid_ident
+from turtle import done
+from webbrowser import get
 
 
 
@@ -22,8 +25,8 @@ def calcula_cadenas_binarias(cadena, alfabeto = 'abcdefghijklmnopqrstuvwxyz'):
         cadenas.append(cadena)
     return cadenas
 
-CLAUS = namedtuple('Point3', ['terminos', 'nivel', 'negado'])
 def get_tuplas_clausulas_list(cadena, variables  ='abcdefghijklmnopqrstuvwxyz' ):
+    CLAUS = namedtuple('Point4', ['terminos', 'nivel', 'negado', 'valor'])
     clausulas = []
     nivel = 0
     clausulas_pendientes = -1 #stack
@@ -34,7 +37,7 @@ def get_tuplas_clausulas_list(cadena, variables  ='abcdefghijklmnopqrstuvwxyz' )
         if char=='{': # Apertura
             clausulas_pendientes += 1 
             nivel += 1
-            clausulas.append(CLAUS([], nivel, negado))
+            clausulas.append(CLAUS([], nivel, negado, [None]))
             negado = False
         elif char=='}':#Cierre de clausula
             if clausulas_pendientes<0:
@@ -70,5 +73,38 @@ def get_tuplas_clausulas_list(cadena, variables  ='abcdefghijklmnopqrstuvwxyz' )
             raise Exception('Error en la cadena')
     return clausulas
 
-def evaluar_expresion(list_tuplas, cadena_binaria):
+def evaluar_expresion(list_tuplas, cadena_binaria, variables):
+    if len(cadena_binaria)!=len(variables):
+        raise Exception("Hubo un error al ingresar las variables involucradas")
+    variables_valuadas = { variables[i]: True if cadena_binaria[i]=='1' else False for i in range(len(cadena_binaria))}
+    #list_tuplas[0].valor[0] = False
+
+def evaluar_clausula(clausula, lista_clausulas, variables_valuadas):
+    operacion = 'and' if clausula.nivel ==1 else 'or'
+    valuadas = []
+    for termino in clausula.terminos:
+        #Evaluacion de terminos
+        if type(termino)== int:
+            valuadas.append(evaluar_clausula(lista_clausulas[termino], lista_clausulas, variables_valuadas))
+        else:
+            if termino[0]=='~':
+                valuadas.append(not variables_valuadas.get(termino[1]))
+            else:
+                valuadas.append(variables_valuadas.get(termino[0]))
+        #Precipita una respuesta de toda la expresion segun le ultimo valor obtenido y la operacion en la que esta implicada directamente
+        if operacion=='and' and valuadas[-1]==False:
+            return False
+        elif operacion=='or' and valuadas[-1]==True:
+            return True
+    """             
+    #Seccion sustituida por la linea que le sigue
+    if operacion=='and': #AND: Si llego a este punto, todos los valuadas fueron true, porque si no habria retornado en la linea 96
+        return True
+    else: #or: Si llego a este punto significa que no encontro ningun True (hubiera retornado en ln 98), todas son False por tanto toda la expresion lo es
+        return False 
+    """
+    return operacion=='and'
+
+print(evaluar_clausula(get_tuplas_clausulas_list('{{~p}, {q}}')[0], get_tuplas_clausulas_list('{{p}, {q}}'), {'p': 0, 'q': 1}))
+def verifica_si_es_satisfactible(expresion):
     pass
